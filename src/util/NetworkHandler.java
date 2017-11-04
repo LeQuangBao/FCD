f5307e69533e5102ed61a257616f3a9bf7fe032f
Lê Quang Bảo <lequangbao13351@gmail.com>
2017-11-04 21:31:09 +0700

    add static to network handler

--- src/util/NetworkHandler.java
+++ src/util/NetworkHandler.java
@@ -9,7 +9,7 @@ import model.Sensor;
 import model.WSN;
 
 public class NetworkHandler {
-	public boolean hasAdjacentNode(WSN wsn, Sensor sensor) {
+	public static boolean hasAdjacentNode(WSN wsn, Sensor sensor) {
 		HashSet<Channel> channels = wsn.getChannels();
 		for (Channel c : channels) {
 			if (c.getFirstSensor() == sensor) {
@@ -19,7 +19,7 @@ public class NetworkHandler {
 		return false;
 	}
 
-	public HashSet<Sensor> getAdjacentNode(WSN wsn, Sensor sensor) {
+	public static HashSet<Sensor> getAdjacentNode(WSN wsn, Sensor sensor) {
 		HashSet<Channel> channels = wsn.getChannels();
 		HashSet<Sensor> hashSensor = new HashSet<>();
 		for (Channel c : channels) {
@@ -30,7 +30,7 @@ public class NetworkHandler {
 		return hashSensor;
 	}
 	
-	public HashSet<Sensor> getAdjacentNode(WSN cluster, WSN original) {
+	public static HashSet<Sensor> getAdjacentNode(WSN cluster, WSN original) {
 		HashSet<Channel> channels = original.getChannels();
 		HashSet<Sensor> adjacentNodes = new HashSet<>();
 		for (Channel c : channels) {
@@ -42,6 +42,33 @@ public class NetworkHandler {
 			}
 		}
 		return adjacentNodes;
+	}
+	public static HashSet<Sensor> getImportantNode(WSN cluster, WSN original) {
+		HashSet<Channel> channels = original.getChannels();
+		HashSet<Sensor> importantNodes = new HashSet<>();
+		for (Sensor s : cluster.getSensors()) {
+			if (s.getsType() == 1 || s.getsType() == 2) {
+				importantNodes.add(s);
+			}
+		}
+		for (Channel c : channels) {
+			if (cluster.hasSensor(c.getFirstSensor()) && !cluster.hasSensor(c.getSecondSensor())) {
+				importantNodes.add(c.getSecondSensor());
+			}
+			if (!cluster.hasSensor(c.getFirstSensor()) && cluster.hasSensor(c.getSecondSensor())) {
+				importantNodes.add(c.getFirstSensor());
+			}
+		}
+		return importantNodes;
+	}
+	
+	public static Sensor findSensorByName(String name, HashSet<Sensor> sensors) {
+		for (Sensor s : sensors) {
+			if (s.getName().equals(name)) {
+				return s;
+			}
+		}
+		return null;
 	}
 
 }
