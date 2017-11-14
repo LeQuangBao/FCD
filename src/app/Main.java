68e007b381425b7ff9e6202501b4d0442cefead6
Lê Quang Bảo <lequangbao13351@gmail.com>
2017-11-14 07:03:40 +0700

    rebuild network part 1

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,8 +12,8 @@ import util.WriteXMLFile;
 
 public class Main {
 	public static void main(String[] args) {
-//		step1();
-		step0();
+		buildTopology();
+//		step0();
 	}
 	
 	public static void step0() {
@@ -25,11 +25,16 @@ public class Main {
 		NetworkHandler.findPath(wsn, s1, s2);
 	}
 	
-	public static void step1() {
-		ReadXMLFile readXMLFile = new ReadXMLFile();
-		WSN original = readXMLFile.readFile("input\\WSN\\original4.kwsn");
-		WSN cluster = readXMLFile.readFile("input\\WSN\\cluster4.kwsn");
+	public static void buildTopology() {
 		
+		ReadXMLFile readXMLFile = new ReadXMLFile();
+		WSN original = readXMLFile.readFile("input\\WSN\\original.kwsn");
+		WSN cluster = readXMLFile.readFile("input\\WSN\\cluster1.kwsn");
+		WSN result = gatherNetwork(original, cluster);
+		WriteXMLFile.write(result, "input\\WSN\\result.kwsn");
+	}
+	
+	public static WSN gatherNetwork(WSN original, WSN cluster) {
 		Sensor mainSensor = NetworkHandler.gather(cluster.getSensors());
 		HashSet<Sensor> adjacentNode = NetworkHandler.getAdjacentNode(cluster, original);
 		
@@ -60,20 +65,49 @@ public class Main {
 				inChannel.setSecondSensor(mainSensor);
 				NetworkHandler.setChannelName(inChannel);
 				mainSensor.addChannels(inChannel);
-//				System.out.println(inChannel.getId() + " -- in -- " + inChannel.getDelay());
 			}
 			if (outChannel != null) {
 				outChannel.setFirstSensor(mainSensor);
 				NetworkHandler.setChannelName(outChannel);
 				mainSensor.addChannels(outChannel);
-//				System.out.println(outChannel.getId() + " -- out -- " + outChannel.getDelay());
 			}
 		}
-		HashSet<Sensor> allNode = (HashSet<Sensor>) adjacentNode.clone();
-		allNode.add(mainSensor);
-		WSN result = new WSN (cluster.getNetwork(), cluster.getProcess(), allNode, mainSensor.getChannels());
+//		HashSet<Sensor> allOriginalSensor = original.getSensors();
+//		HashSet<Sensor> allClusterSensor = cluster.getSensors();
+		HashSet<Sensor> allSensor = new HashSet<>();
+		allSensor = original.getSensors();
+//		
+//		for (Sensor s : allOriginalSensor) {
+//			boolean same = false;
+//			for (Sensor s2 : allClusterSensor) {
+//				if (s.is(s2)) {
+//					same = true;
+//				}	
+//			}
+//			if (!same) {
+//				allSensor.add(s);
+//			}
+//		}
+//		allSensor.add(mainSensor);
+//		
+//		HashSet<Channel> allOriginalChannel = original.getChannels();
+//		HashSet<Channel> allClusterChannel = cluster.getChannels();
+		HashSet<Channel> allChannel = new HashSet<>(original.getChannels());
+		
+//		for (Channel c : allOriginalChannel) {
+//			for (Channel c2 : allClusterChannel) {
+//				if (!c2.getFirstSensor().is(c.getFirstSensor()) || !c2.getSecondSensor().is(c.getSecondSensor())) {
+//					allChannel.add(c);
+//				}	
+//			}
+//		}
+		for (Channel c : mainSensor.getChannels()) {
+			allChannel.add(c);
+		}
+		WSN result = new WSN (cluster.getNetwork(), cluster.getProcess(), allSensor, allChannel);
 		
 		
-		WriteXMLFile.write(result, "output\\result.kwsn");
+		return result;
 	}
+	
 }
